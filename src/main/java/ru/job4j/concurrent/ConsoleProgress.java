02d397621fa0ccb71cc854579dc578911c01eda4
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2021-02-28 15:29:23 +0300

    3. Прерывание нити[1019#482354]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,32 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    private static char[] PROGRESS = { '\\', '|', '/', '-' };
+
+    @Override
+    public void run() {
+        int i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + PROGRESS[i%PROGRESS.length]);
+            i++;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args)  {
+        Thread t1 = new Thread(new ConsoleProgress());
+        t1.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t1.interrupt();
+        System.out.println("");
+        System.out.print("Finish");
+    }
+}
