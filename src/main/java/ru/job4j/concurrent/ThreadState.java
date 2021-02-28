995d254d7385f9e8c611972644ecfdde12490f78
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2021-02-28 14:54:55 +0300

    1.1. Состояние нити.[229175#482350]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,16 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED);
+        System.out.println("Finished");
+    }
+}
