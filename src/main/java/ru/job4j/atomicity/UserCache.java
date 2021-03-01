c184102e750ec1d1bd2554479e04255e39761dee
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2021-03-01 11:00:06 +0300

    4. Thread без общих ресурсов[267919#482359]

---
+++ src/main/java/ru/job4j/atomicity/UserCache.java
@@ -0,0 +1,28 @@
+package ru.job4j.atomicity;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@ThreadSafe
+public class UserCache {
+    private final Map<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        return users.values().stream().map(u -> User.of(u.getName())).collect(Collectors.toList());
+    }
+}
