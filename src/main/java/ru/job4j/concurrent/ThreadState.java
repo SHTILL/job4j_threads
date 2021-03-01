9e6966e2aaca399c9624a73c165a26b61fd9d4d8
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2021-03-01 09:25:19 +0300

    1.1. Состояние нити.[229175#482350]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -10,7 +10,7 @@ public class ThreadState {
         );
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED);
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED);
         System.out.println("Finished");
     }
 }
