6cfecfeae1abc0a0b770fff3624895f83ef18546
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2021-03-11 12:42:23 +0300

    style

--- src/main/java/ru/job4j/interaction/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/interaction/SimpleBlockingQueue.java
@@ -1,22 +1,24 @@
 package ru.job4j.interaction;
 
 import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
+@ThreadSafe
 public class SimpleBlockingQueue<T> {
     @GuardedBy("this")
     private Queue<T> queue = new LinkedList<>();
 
-    synchronized public void offer(T value) throws InterruptedException {
+    public synchronized void offer(T value) throws InterruptedException {
         while (!queue.offer(value)) {
             this.wait();
         }
         this.notifyAll();
     }
 
-    synchronized public T poll() throws InterruptedException {
+    public synchronized T poll() throws InterruptedException {
         T item;
         while ((item = queue.poll()) == null) {
             this.wait();
